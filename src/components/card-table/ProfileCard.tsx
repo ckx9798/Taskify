491f37c5f38b52e017c533cad4098a8461a8358a
ckx9798 <ckx12@Naver.com>
2024-10-24 20:57:51 +0900

    Feat ProfileCard 이미지 선택 로직 추가

--- src/components/card-table/ProfileCard.tsx
+++ src/components/card-table/ProfileCard.tsx
@@ -1,6 +1,26 @@
 import Image from "next/image";
+import React, { useRef, useState } from "react";
 
 export default function ProfileCard() {
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  // 이미지 파일 선택 창 열기
+  const handleProfileImg = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  // 이미지 파일 선택
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setProfileImage(imageUrl);
+    }
+  };
+
   return (
     <>
       <div
@@ -13,28 +33,50 @@ export default function ProfileCard() {
         </div>
 
         <div className={"flex gap-11"}>
-          <div
+          <button
             className={
-              "w-44 min-w-[182px] h-44 bg-gray-300 flex justify-center items-center"
+              "w-44 min-w-[182px] h-44 bg-gray-300 rounded-md flex justify-center items-center text-gray-100"
             }
+            onClick={handleProfileImg}
           >
-            <Image src="/svg/add_box.svg" alt="+" width={30} height={30} />
-          </div>
-          <form className={"w-full max-w-[400px]  flex flex-col gap-6"}>
-            <div>
-              <h3>이메일</h3>
+            {profileImage ? (
+              <img
+                src={profileImage}
+                alt="프로필 이미지"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            ) : (
+              <Image src="/svg/add_box.svg" alt="+" width={30} height={30} />
+            )}
+          </button>
+          <input
+            type="file"
+            ref={fileInputRef}
+            className={"hidden"}
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+
+          <form className={"w-full max-w-[400px] flex flex-col gap-6"}>
+            <div className={"flex flex-col gap-2"}>
+              <label htmlFor="email">이메일</label>
               <input
-                className={"w-full h-12 p-4 rounded-lg border border-gray-400"}
+                className={
+                  "w-full h-12 p-4 rounded-lg border border-gray-400 bg-inherit"
+                }
                 type="email"
                 placeholder="asd@naver.com"
+                id="email"
+                disabled
               />
             </div>
-            <div>
-              <h3>닉네임</h3>
+            <div className={"flex flex-col gap-2"}>
+              <label htmlFor="nickname">닉네임</label>
               <input
                 className={"w-full h-12 p-4 rounded-lg border border-gray-400"}
-                type="email"
+                type="text"
                 placeholder="배유철"
+                id="nickname"
               />
             </div>
             <div
@@ -42,7 +84,7 @@ export default function ProfileCard() {
                 "w-full h-12 p-4 rounded-lg bg-blue-700 flex justify-center items-center text-white font-semibold"
               }
             >
-              <button> 저장 </button>
+              <button type="submit"> 저장 </button>
             </div>
           </form>
         </div>
--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,9 +1,19 @@
+import ProfileCard from "@/components/card-table/ProfileCard";
 import SideMenu from "@/components/modal/SideMenu";
+import ChangePwCard from "@/components/card-table/ChangePwCard";
 
 export default function mypage() {
   return (
-    <>
+    <div className={"flex"}>
       <SideMenu />
-    </>
+
+      <div>
+        <div>네비바</div>
+        <div className={"w-screen h-full bg-gray-400 p-5 flex flex-col gap-7"}>
+          <ProfileCard />
+          <ChangePwCard />
+        </div>
+      </div>
+    </div>
   );
 }
