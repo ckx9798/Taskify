4353c1e01a67357654e2d5ff0a22953290c89430
Wooy <wjddndud5023@gmail.com>
2024-10-24 14:33:23 +0900

    NavBar BoxButton props 수정

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,8 +7,8 @@ export default function NavBar() {
     <nav className="bg-white">      
       <div className="flex items-center">
         <div className="flex gap-x-1.5 mr-4">
-          <BoxButton width="88" height="40" radius="8" backgroundColor="white" fontSize="16" disabled={false} onClick={() => {alert('관리')}}>관리</BoxButton>
-          <BoxButton width="116" height="40" radius="8" backgroundColor="white" fontSize="16" disabled={false} onClick={() => {alert('초대하기')}}>초대하기</BoxButton>
+          <BoxButton paddingTopBottom="7" paddingRightLeft="12" radius="8" backgroundColor="white" fontSize="16" disabled={false} onClick={() => {alert('관리')}}>관리</BoxButton>
+          <BoxButton paddingTopBottom="7" paddingRightLeft="12" radius="8" backgroundColor="white" fontSize="16" disabled={false} onClick={() => {alert('초대하기')}}>초대하기</BoxButton>
         </div>
         <div className="flex -space-x-2 mr-8">
           <ProfileImage>Y</ProfileImage>
