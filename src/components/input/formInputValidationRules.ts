9a2c9465de68ef57713d4748f8e70852bc162709
imtaejunk <imtaejunk@gmail.com>
2024-10-24 12:59:50 +0900

    실수로 다른 브랜치에서 작업한 내용

---
+++ src/components/input/formInputValidationRules.ts
@@ -0,0 +1,47 @@
+export const emailReg =
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
+
+export const passwordReg = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+
+export const nicknameReg = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/;
+
+export const emailValidationRules = {
+  required: "이메일은 필수 입력입니다.",
+  pattern: {
+    value: emailReg,
+    message: "이메일 형식으로 작성해 주세요.",
+  },
+};
+
+export const passwordValidationRules = {
+  required: "비밀번호는 필수 입력입니다.",
+  minLength: {
+    value: 8,
+    message: "8자리 이상 입력해 주세요.",
+  },
+  pattern: {
+    value: passwordReg,
+    message:
+      "비밀번호는 영어 대, 소문자와 숫자가 각각 1개 이상 포함되게 해주세요.",
+  },
+};
+
+export const noRequiredNicknameValidationRules = {
+  maxLength: {
+    value: 10,
+    message: "열 자 이하로 작성해 주세요.",
+  },
+  pattern: {
+    value: nicknameReg,
+    message: "닉네임에는 공백이나 특수문자가 들어갈 수 없어요.",
+  },
+};
+
+export const nicknameValidationRules = {
+  required: "닉네임은 필수 입력입니다.",
+  ...noRequiredNicknameValidationRules,
+};
+
+export const imgFileValidationRules = {};
+
+// 정규식을 모아두는 파일
