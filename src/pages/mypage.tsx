1b410fcbb786ef544af54a98db214b3ca5df38f3
ckx9798 <ckx12@Naver.com>
2024-10-25 12:06:57 +0900

    Feat mypage 생성

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,8 +1,17 @@
 import ProfileCard from "@/components/card-table/ProfileCard";
 import SideMenu from "@/components/modal/SideMenu";
 import ChangePwCard from "@/components/card-table/ChangePwCard";
+import { updateUserInfo } from "@/libs/api/Users";
 
 export default function mypage() {
+  const handliClickSave = async ({ nickname, profileImageUrl }) => {
+    //api
+    const data = await updateUserInfo({
+      nickname: nickname,
+      profileImageUrl: profileImageUrl,
+    });
+    console.log(data);
+  };
   return (
     <div className={"flex"}>
       <SideMenu />
@@ -10,7 +19,7 @@ export default function mypage() {
       <div>
         <div>네비바</div>
         <div className={"w-screen h-full bg-gray-400 p-5 flex flex-col gap-7"}>
-          <ProfileCard />
+          <ProfileCard updateUserProfile={handliClickSave} />
           <ChangePwCard />
         </div>
       </div>
