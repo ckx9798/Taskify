3bf17cceb47508c421652f59047a1301fc706ea4
ckx9798 <ckx12@Naver.com>
2024-10-31 08:20:15 +0900

    Feat 마이페이지 - 프로필 수정 카드 컴포넌트 구현

--- src/components/card-table/ProfileCard.tsx
+++ src/components/card-table/ProfileCard.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
-import React, { FormEvent, useRef, useState } from "react";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
+import MypageInput from "./MypageInput";
+import { createUserProfileImg, getUserInfo } from "@/libs/api/Users";
+import ErrorMessageModal from "./ErrorMessageModal";
+import axios from "axios";
+import ProfileImageUploader from "./ProfileImageUploader";
 
 interface ProfileCardProps {
   updateUserProfile: (userData: {
@@ -9,20 +13,72 @@ interface ProfileCardProps {
 }
 
 export default function ProfileCard({ updateUserProfile }: ProfileCardProps) {
+  const [selectedProfileFile, setSelectedProfileFile] = useState<File | null>(
+    null,
+  );
   const [profileImage, setProfileImage] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const [nickName, setNickName] = useState("전역");
-  const changeNickName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNickName(e.target.value);
+  const [nickName, setNickName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [nicknameError, setNicknameError] = useState("");
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  // 유저정보 가져오는 요청 api
+  const setUserInfo = async () => {
+    try {
+      const data = await getUserInfo();
+      if (data && data.data) {
+        setProfileImage(data.data.profileImageUrl);
+        setNickName(data.data.nickname);
+        setUserEmail(data.data.email);
+      } else {
+        console.error("Invalid user data received");
+      }
+    } catch (error) {
+      console.error("Failed to fetch user info", error);
+    }
   };
+  useEffect(() => {
+    setUserInfo();
+  }, []);
+
+  //프로필 정보 수정 요청 api
   const handleClickSave = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    //api
     e.preventDefault();
-    const data = await updateUserProfile({
-      nickname: nickName,
-      profileImageUrl: profileImage,
-    });
+    // 이미지 업로드 요청
+    let profileImg = null;
+    if (selectedProfileFile) {
+      try {
+        const imgdata = await createUserProfileImg(selectedProfileFile);
+        profileImg = imgdata.profileImageUrl;
+      } catch (error) {
+        console.error("Failed to upload image", error);
+      }
+    }
+
+    // 프로필 정보 수정 요청
+    try {
+      // const data =
+      await updateUserProfile({
+        nickname: nickName,
+        profileImageUrl: profileImg,
+      });
+      setSuccessMessage("프로필 수정 성공!");
+      setIsModalOpen(true);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const message =
+          error.response && error.response.data
+            ? error.response.data.message || JSON.stringify(error.response.data)
+            : error.message;
+        setErrorMessage(message);
+        setIsModalOpen(true);
+      }
+    }
   };
 
   // 이미지 파일 선택 창 열기
@@ -31,19 +87,40 @@ export default function ProfileCard({ updateUserProfile }: ProfileCardProps) {
       fileInputRef.current.click();
     }
   };
-
   // 이미지 파일 선택
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setProfileImage(imageUrl);
+      setSelectedProfileFile(file);
     }
   };
 
-  function isNicknameInputError() {
+  const modalClose = (e?: FormEvent) => {
+    e?.preventDefault();
+    setIsModalOpen(false);
+  };
+
+  const isNicknameInputError = () => {
     return nickName.length > 10;
-  }
+  };
+
+  const deleteImg = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setProfileImage(null);
+    setSelectedProfileFile(null);
+  };
+
+  const nicknameValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 10) {
+      setNicknameError("닉네임을 10자 이내로 입력해주세요.");
+    } else {
+      setNicknameError("");
+    }
+    setNickName(e.target.value);
+  };
+
   return (
     <>
       <div
@@ -56,25 +133,14 @@ export default function ProfileCard({ updateUserProfile }: ProfileCardProps) {
         </div>
 
         <div className={"flex gap-11"}>
-          <button
-            className={
-              "flex h-44 w-44 min-w-[182px] items-center justify-center rounded-md bg-gray-200 text-gray-100"
-            }
-            onClick={handleProfileImg}
-          >
-            {profileImage ? (
-              <Image
-                src={profileImage}
-                alt="프로필 이미지"
-                className="h-full w-full rounded-lg object-cover"
-                width={30}
-                height={30}
-              />
-            ) : (
-              <Image src="/svg/violetPlus.svg" alt="+" width={30} height={30} />
-            )}
-          </button>
-
+          {/* 이미지 선택 컴포넌트  */}
+          <ProfileImageUploader
+            profileImage={profileImage}
+            handleProfileImg={handleProfileImg}
+            handleFileChange={handleFileChange}
+            deleteImg={deleteImg}
+            fileInputRef={fileInputRef}
+          />
           <input
             type="file"
             ref={fileInputRef}
@@ -82,52 +148,58 @@ export default function ProfileCard({ updateUserProfile }: ProfileCardProps) {
             accept="image/*"
             onChange={handleFileChange}
           />
-
-          <form className={"flex w-full max-w-[400px] flex-col gap-6"}>
+          <form className={"flex w-full max-w-[400px] flex-col gap-3"}>
             {/* 이메일 */}
             <div className={"flex flex-col gap-2"}>
-              <label htmlFor="email">이메일</label>
-              <input
-                className={
-                  "h-12 w-full rounded-lg border border-gray-400 bg-inherit p-4 placeholder-black"
-                }
-                type="email"
-                placeholder="asd@naver.com"
-                id="email"
-                disabled
+              <MypageInput
+                inputText="이메일"
+                inputType="text"
+                value={userEmail}
+                labelId="email"
+                error=""
+                disabled={true}
               />
             </div>
 
             {/* 닉네임 */}
-            <div className={"flex flex-col gap-2"}>
-              <label htmlFor="nickname">닉네임</label>
-              <input
-                className={"h-12 w-full rounded-lg border border-gray-400 p-4"}
-                type="text"
-                placeholder="배유철"
-                id="nickname"
+            <div className={"flex flex-col"}>
+              <MypageInput
+                inputText="닉네임"
+                inputType="text"
+                labelId="nickname"
                 value={nickName}
-                onChange={changeNickName}
+                error={nicknameError}
+                onChange={nicknameValidation}
               />
-              <div className={`${!isNicknameInputError() ? "hidden" : ""}`}>
-                닉네임을 10자 이내로 입력해주세요
-              </div>
+              <p className={"mt-2 text-sm text-red"}>{nicknameError}</p>
             </div>
 
+            {/* 저장버튼 */}
             <label
               className={
-                "flex h-12 w-full items-center justify-center rounded-lg bg-blue p-4 font-semibold text-white"
+                "mt-4 flex h-12 w-full items-center justify-center rounded-lg bg-blue p-4 font-semibold text-white"
               }
-              htmlFor="clickButton"
+              htmlFor="저장버튼"
             >
               <button
                 type="submit"
                 onClick={handleClickSave}
                 disabled={isNicknameInputError()}
+                id="저장버튼"
               >
                 저장
               </button>
             </label>
+            {/* 에러 메시지 보여주는 모달 */}
+            {isModalOpen && (
+              <ErrorMessageModal
+                errorMessage={errorMessage}
+                successMessage={successMessage}
+                width={"368px"}
+                height={"192px"}
+                modalClose={modalClose}
+              />
+            )}
           </form>
         </div>
       </div>
