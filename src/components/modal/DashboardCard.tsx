e52662c29d7791de2536644f99c77f76160266a1
ckx9798 <ckx12@Naver.com>
2024-11-01 10:30:09 +0900

    Style 사이드바 반응현 구현

--- src/components/modal/DashboardCard.tsx
+++ src/components/modal/DashboardCard.tsx
@@ -16,17 +16,19 @@ const DashboardCard: React.FC<DashboardCardProps> = ({
   isArrow = false,
 }) => {
   return (
-    <div className="flex items-center justify-between p-5">
-      <div className="flex items-center gap-3">
-        <div
+    <div className="flex items-center justify-center px-2 py-5 md:justify-between xl:px-5">
+      <div className="flex items-center gap-6">
+        <span
           className="flex h-2 w-2 items-center justify-center rounded-full"
           style={{ backgroundColor: dashboard.color }}
         />
-        <span className="text-lg font-medium text-gray-500">
+        <span className="hidden font-medium text-gray-500 md:inline md:text-base xl:text-lg">
           {dashboard.title}
         </span>
         {dashboard.createdByMe ? (
-          <Image src="/svg/crown.svg" width={17} height={14} alt="Crown" />
+          <div className={"hidden md:inline"}>
+            <Image src="/svg/crown.svg" width={17} height={14} alt="Crown" />
+          </div>
         ) : null}
       </div>
       {isArrow ? (
